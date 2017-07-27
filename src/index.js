37744ae3c7dd63b861dbc782f36dc7bda8f69df6
Sahngah Lee <sahngah.lee@gmail.com>
2017-07-26 18:00:54 -0700

    started writing  code for ajax requests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'React-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchBar';
-
+import VideoList from './components/videoList';
 
  // Create a new component that produces some HTML
 
- const App = () => {
-   return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+ class App extends Component {
+   constructor(props) {
+     super(props);
+
+     this.state = { videos: [] };
+
+     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+       this.setState({ videos });
+       // this.setState({ videos: videos }) in ES5
+     });
+   }
+   render () {
+     return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
  }
+
  // Take this component's generated HTML and put it on the browser page (on the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
